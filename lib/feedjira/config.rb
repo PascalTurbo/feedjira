e028f64373ec960efcae075d29c62a2563d1f1c1
Christian Becker <christian@beckerchristian.com>
2016-12-19 20:31:06 +0100

    Added code documentation to logger and config (#312,#340)

--- lib/feedjira/config.rb
+++ lib/feedjira/config.rb
@@ -1,13 +1,18 @@
+# Feedjira::Config
 module Feedjira
-  # Set configuration options by block
-  # Feedjira.configure do |config|
-  #   config.logger = my_logger_instance
-  # end
-
   def self.configure
     yield Config
   end
 
+  # Provides global configuration options for Feedjira
+  #
+  # @example Set configuration options using a block
+  #   Feedjira.configure do |config|
+  #     config.logger = my_logger_instance
+  #   end
+  #
+  # @example Set configuration options directly
+  #   Feedjira::Config.logger = Logger.new('foo.log')
   module Config
     DEFAULTS = {
       logger_io: STDOUT,
@@ -15,10 +20,20 @@ module Feedjira
     }.freeze
 
     class << self
+      # Set the global logger
+      # @see Feedjira::Logger
+      #
+      # @param logger [Logger] a ruby Logger
+      #
+      # @return [Logger]
       def logger=(logger)
         Feedjira.logger = logger
       end
 
+      # Get the global logger
+      # @see Feedjira::Logger
+      #
+      # @return [Logger]
       def logger
         Feedjira.logger
       end
--- lib/feedjira/logger.rb
+++ lib/feedjira/logger.rb
@@ -1,9 +1,14 @@
+# Feedjira.logger
 module Feedjira
   class << self
     attr_writer :logger
 
     # Provides a global logger instance for Feedjira
-    # If no logger is set by the user, a logger using STDOUT is provided
+    # If no logger is set by the user, a logger using DEFAULTS is provided
+    # @see Feedjira::Config::DEFAULTS
+    # @see Feedjira::Logger
+    #
+    # @return [Logger]
     def logger
       return @logger unless @logger.nil?
       @logger = ::Logger.new(Feedjira::Config::DEFAULTS[:logger_io])
@@ -18,28 +23,51 @@ module Feedjira
   #   Feedjira::Logger.info { "My Info Message" }
   module Logger
     class << self
+      # Log a message given as a block if level DEBUG is set
+      #
+      # @yieldreturn [String] The Message to log
       def debug
         Feedjira.logger.debug('Feedjira') { yield }
       end
 
+      # Log a message given as a block if level INFO is set
+      #
+      # @yieldreturn [String] The Message to log
       def info
         Feedjira.logger.info('Feedjira') { yield }
       end
 
+      # Log a message given as a block if level WARN is set
+      #
+      # @yieldreturn [String] The Message to log
       def warn
         Feedjira.logger.warn('Feedjira') { yield }
       end
 
+      # Log a message given as a block if level ERROR is set
+      #
+      # @yieldreturn [String] The Message to log
       def error
         Feedjira.logger.error('Feedjira') { yield }
       end
 
+      # Log a message given as a block if level FATAL is set
+      #
+      # @yieldreturn [String] The Message to log
       def fatal
         Feedjira.logger.fatal('Feedjira') { yield }
       end
 
       # Log exceptions with message and backtrace in a common way
-      # Exceptions will only be logged when severity level 'debug' is set
+      # Exceptions will only be logged when severity level 'DEBUG' is set
+      #
+      # @example Log an Exception with a custom message
+      #   Feedjira::Logger.exception(StandardError.new) {
+      #     'Log this great message'
+      #   }
+      #
+      # @param e [Exception] The exception to log
+      # @yieldreturn [String] The Message to log (optional)
       def exception(e)
         Feedjira.logger.debug('Feedjira') do
           msg = block_given? ? "#{yield}\n" : ''
